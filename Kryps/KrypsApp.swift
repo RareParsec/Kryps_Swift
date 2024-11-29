45aeb5b53bc2fa2c3c8b8b9180f5c2a255310e88
RareParsec <dhillonaaspreet@gmail.com>
2024-11-28 20:19:21 -0800

    Initial Commit

---
+++ Kryps/KrypsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KrypsApp.swift
+//  Kryps
+//
+//  Created by Aaspreet Dhillon on 2024-11-28.
+//
+
+import SwiftUI
+
+@main
+struct KrypsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
